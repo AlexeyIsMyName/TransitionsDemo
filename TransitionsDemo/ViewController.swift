263a015dda70323a00e83cb499d9325a76e9a508
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-14 18:22:38 +0300

    made Rick & Morty view with Rick & Morty Button. Title for view and button get from a property text in VC

--- TransitionsDemo/ViewController.swift
+++ TransitionsDemo/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var justButton: UIButton!
+    
+    let text = "Rick & Morty 🤘🏻"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = text
+        
+        let attributes: [NSAttributedString.Key : Any]? = [
+            .font : UIFont.init(name: "Helvetica", size: 30.0)!
+        ]
+        
+        let title = NSAttributedString(string: text, attributes: attributes)
+        
+        justButton.setAttributedTitle(title, for: .normal)
     }
 
 
